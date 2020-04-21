7d26770aad5a2ee4687b85bd8b2bb55e761f6374
Russell Kerber <russell.kerber@gmail.com>
2020-04-21 17:22:57 -0500

    user onCreate function almost works

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,8 +1,30 @@
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
+admin.initializeApp();
+//const admin = require('firebase-admin');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+
+//const usersRef = admin.database().ref('/users');
+
 exports.helloWorld = functions.https.onCall((data, context) => {
   return 'Hello from Firebase!';
 });
+
+exports.addUser = functions.auth.user().onCreate(user => {
+  admin.database().ref('users').push({
+    [user.uid]: {
+      isBanned: 0
+    }
+  })
+});
+// exports.addUser = functions.https.onCall((data, context) => {
+//   console.log('in addUser function');
+//   usersRef.push({
+//     [data.uid]: {
+//       isBanned: 0,
+//       favoriteImage: '',
+//     },
+//   });
+// });
