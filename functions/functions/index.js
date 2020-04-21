6edc2a2cd58233e461535194b3f8d722685af52e
Russell Kerber <russell.kerber@gmail.com>
2020-04-21 18:28:25 -0500

    new functions folder created without eslint

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,32 +1,8 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp();
-//const admin = require('firebase-admin');
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-
-//const usersRef = admin.database().ref('/users');
-
-exports.helloWorld = functions.https.onCall((data, context) => {
-  return 'Hello from Firebase!';
-});
-
-exports.addUser = functions.auth.user().onCreate(user => {
-  admin
-    .database()
-    .ref(`users/${user.uid}`)
-    .set({
-      isBanned: 0,
-      email: user.email,
-    });
-});
-// exports.addUser = functions.https.onCall((data, context) => {
-//   console.log('in addUser function');
-//   usersRef.push({
-//     [data.uid]: {
-//       isBanned: 0,
-//       favoriteImage: '',
-//     },
-//   });
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
 // });
