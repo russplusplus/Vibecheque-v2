ddfbb553bb1774d4acf49115a116b2c01716464e
Russell Kerber <russell.kerber@gmail.com>
2020-04-21 18:32:58 -0500

    functions work without eslint

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
+admin.initializeApp();
+//const admin = require('firebase-admin');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+//const usersRef = admin.database().ref('/users');
+
+exports.helloWorld = functions.https.onCall((data, context) => {
+  return 'Hello from Firebase!';
+});
+
+exports.addUser = functions.auth.user().onCreate(user => {
+  admin
+    .database()
+    .ref(`users/${user.uid}`)
+    .set({
+      isBanned: 0,
+      email: user.email,
+    });
+});
