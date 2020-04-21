5852135fc37cd3adfec7efa68edd1ee3f8d0d68c
Russell Kerber <russell.kerber@gmail.com>
2020-04-21 18:51:10 -0500

    deleted comments in functions index.js

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,12 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
-//const admin = require('firebase-admin');
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-
-//const usersRef = admin.database().ref('/users');
 
 exports.helloWorld = functions.https.onCall((data, context) => {
   return 'Hello from Firebase!';
@@ -21,3 +15,5 @@ exports.addUser = functions.auth.user().onCreate(user => {
       email: user.email,
     });
 });
+
+
